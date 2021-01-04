5309f67607b1e399677ed9c60eb48e90e7d1fa9f
Indra Purnomo <indra.purnomo@mail.ugm.ac.id>
2021-01-04 12:28:12 +0700

    Update ec2.tf

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
+    aws       = {
+      source  = "hashicorp/aws"
       version = "3.13.0"
     }
   }
@@ -33,28 +33,28 @@ EOF
 #############################################################
 
 resource "aws_instance" "test-instance" {
-   count			                    = "1" 
-   ami                          	= "ami-0dd9f0e7df0f0a138"
-   tenancy                      	= "default"
-   associate_public_ip_address  	= true 
-   availability_zone            	= "us-east-2a"
-   key_name		                    = "indrakey"
-   instance_type                	= "t2.micro" 
-   monitoring                   	= true
+   count                            = "1" 
+   ami                              = "ami-0dd9f0e7df0f0a138"
+   tenancy                          = "default"
+   associate_public_ip_address      = true 
+   availability_zone                = "us-east-2a"
+   key_name                         = "indrakey"
+   instance_type                    = "t2.micro" 
+   monitoring                       = true
 
-   subnet_id				                = "subnet-079e83aeeb6758e96" //subnet VPC
+   subnet_id                        = "subnet-079e83aeeb6758e96" //subnet VPC
    vpc_security_group_ids           = [
             "sg-016ef4528278ccd30"                               //Security group VPC
        ]
    
-   tags                         	= {
+   tags = {
      		Name	= "test-instance"
         } 
            
    root_block_device {
-           delete_on_termination 	= true 
-           volume_size           	= 10 
-           volume_type           	= "gp2" 
+           delete_on_termination 	  = true 
+           volume_size           	  = 10 
+           volume_type           	  = "gp2" 
         }
-   user_data_base64			= base64encode(local.user_data)
+   user_data_base64			            = base64encode(local.user_data)
 }
